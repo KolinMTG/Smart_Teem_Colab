6a0c17a949bb5407ae7e59e40dcefe9b0d4f5680
lojain <lojainrhafiri@gmail.com>
2025-06-16 23:04:25 +0200

    changement vue medicament by pathology

--- sql/_views/vw_top_medication_by_pathology.sql
+++ sql/_views/vw_top_medication_by_pathology.sql
@@ -2,9 +2,17 @@ CREATE OR REPLACE VIEW BASE_SOCLE.PUBLIC.vw_medication_by_pathology AS
 SELECT 
     c.PATH_DSC AS pathology,
     t.MEDC_ID AS medication_id,
-    t.MEDC_QTY AS quantity,
-    t.TRET_CRTN_DTTM AS prescription_date
-FROM BASE_SOCLE.PUBLIC.TREATMENT t
-JOIN BASE_SOCLE.PUBLIC.CONSULTATION c 
-    ON t.CONS_ID = c.CONS_ID
-WHERE c.PATH_DSC IS NOT NULL;
+    m.MEDC_NAME AS medication_name,
+    DATE(t.TRET_CRTN_DTTM) AS prescription_date,
+    SUM(t.MEDC_QTY) AS total_quantity
+FROM
+    BASE_SOCLE.PUBLIC.TREATMENT t
+JOIN
+    BASE_SOCLE.PUBLIC.CONSULTATION c ON t.CONS_ID = c.CONS_ID
+JOIN
+    BASE_SOCLE.PUBLIC.MEDICINE m ON t.MEDC_ID = m.MEDC_ID
+GROUP BY
+    c.PATH_DSC,
+    t.MEDC_ID,
+    m.MEDC_NAME,
+    DATE(t.TRET_CRTN_DTTM);
