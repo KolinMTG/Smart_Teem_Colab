80ce1003b94caaf1783b9ab12d7ede8846a963ba
nguillau <nadia.guillaumot@etu.utc.fr>
2025-06-17 11:24:31 +0200

    correction des vues

--- sql/_views/vw_patients_one_night.sql
+++ sql/_views/vw_patients_one_night.sql
@@ -1,7 +1,16 @@
 CREATE OR REPLACE VIEW BASE_SOCLE.PUBLIC.vw_hospital_stays AS
 SELECT 
-    h.CONS_ID,
-    h.HOSP_STRT_DTTM AS start_date,
-    h.HOSP_END_DTTM AS end_date,
-    DATEDIFF(day, h.HOSP_STRT_DTTM, h.HOSP_END_DTTM) AS duration_days
-FROM BASE_SOCLE.PUBLIC.HOSPITALIZATION h;
+    c.CONS_ID,
+    c.CONS_STRT_DTTM AS consultation_date,
+    c.PATN_ID AS patient_id,
+    h.HOSP_STRT_DTTM AS hosp_start_date,
+    h.HOSP_END_DTTM AS hosp_end_date,
+    CASE 
+        WHEN h.HOSP_STRT_DTTM IS NOT NULL 
+        AND DATEDIFF(day, h.HOSP_STRT_DTTM, h.HOSP_END_DTTM) >= 1 
+        THEN TRUE
+        ELSE FALSE
+    END AS stayed_at_least_one_night
+FROM BASE_SOCLE.PUBLIC.CONSULTATION c
+LEFT JOIN BASE_SOCLE.PUBLIC.HOSPITALIZATION h 
+    ON c.CONS_ID = h.CONS_ID;
